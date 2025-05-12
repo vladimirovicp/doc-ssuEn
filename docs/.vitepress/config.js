d090eef6bd6f39b3ba9c65949c7b49b532c7e843
vladimirovicp <vladimirovicp70@gmail.com>
2025-05-12 11:27:49 +0400

    feat: подготовка

---
+++ docs/.vitepress/config.js
@@ -0,0 +1,39 @@
+const doc = [
+    { text: 'Promo', link: '/doc/promo/'},
+];
+
+
+module.exports = {
+    lang: 'ru-RU',
+    title: 'doc-SSU-EN',
+    description: 'Документация для сайта ssu En',
+    // logo: '/logo.png',
+    themeConfig: {
+        nav: [
+            {text: 'Главная', link: '/'},
+            { text: 'Документация', link: '/doc/'},
+            { text: 'Контакты', link: '/contact/'}
+        ],
+        sidebar: {
+            '/doc/': doc,
+        },
+
+        outline: { label: 'Содержание страницы' },
+        docFooter: {
+            prev: 'Предыдущая страница',
+            next: 'Следующая страница'
+        },
+        footer: {
+            message: 'Опубликовано под лицензией ???.',
+            copyright: '© 2025'
+        },
+
+        darkModeSwitchLabel: 'Оформление',
+        lightModeSwitchTitle: 'Переключить на светлую тему',
+        darkModeSwitchTitle: 'Переключить на тёмную тему',
+        sidebarMenuLabel: 'Меню',
+        returnToTopLabel: 'Вернуться к началу',
+        langMenuLabel: 'Изменить язык',
+        skipToContentLabel: 'Перейти к содержимому'
+    },
+}
---
+++ docs/index.md
@@ -0,0 +1,22 @@
+---
+layout: home
+
+hero:
+  name: СГУ версия en
+  text: Документация для редакторов
+  tagline: Документация предназначена для быстрого старта редакторов на ресурсе en.sgu.ru
+  actions:
+    - theme: brand
+      text: Перейти на сайт
+      link: https://en.sgu.ru
+    - theme: alt
+      text: Первые шаги
+      link: /ru/guide/getting-started
+    - theme: alt
+      text: Авторизация
+      link: https://en.sgu.ru/user
+  image:
+    src: /screenshot.png
+    alt: СГУ английская версия
+
+---
